50672b1ba55629953440da463d0c4ec2fcbbd0d7
ScanAlign <atifjan2019@gmail.com>
2026-04-13 23:27:59 +0500

    feat: add robots.txt configuration to define crawler access rules and sitemap location

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/"],
     },
-    sitemap: "https://webspires.com.pk/sitemap.xml",
+    sitemap: "https://www.webspires.com.pk/sitemap.xml",
   };
 }
